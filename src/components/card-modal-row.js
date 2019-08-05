c7e06a8ae4ceb5ef917d07ba09cc61a85c651323
Isaac Orme <isorme1@gmail.com>
2019-08-05 11:33:39 -0700

    CardModalRow component changed to functional component

--- src/components/card-modal-row.js
+++ src/components/card-modal-row.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { Card } from '../mapping';
 
-class CardModalRow extends Component {
-  render() {
-    return (
-      <div className="col-2">
-        <img
-          className="my-modal-card"
-          src={`../img/${this.props.src}`}
-          onClick={this.props.setCard}
-        />
-      </div>
-    );
-  }
+const CardModalRow = (props) => {
+  return (
+    <div className="col-2">
+      <img
+        className="my-modal-card"
+        src={`../img/${props.src}`}
+        onClick={props.setCard}
+      />
+    </div>
+  );
 }
 
 export default CardModalRow;
